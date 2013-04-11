946b9d52945ad2c17719ecf4f733716b3a8ad23e
Alicia Lauerman <allydevour@me.com>
2013-04-11 03:17:46 -0400

    started controls section

--- js/main.coffee
+++ js/main.coffee
@@ -1,18 +1,52 @@
 $ ->
-  volumeEl = $('.volume')
+  volumeEl = $('.volume-track')
   level = $('.volume-level')
   handle = $('.volume-handle')
 
   @pagePos = 0
 
-  volumeEl.mousedown (e) =>
-    return unless e.which == 1
-    @dragging = true
-    pagePos = e.pageX - volumeEl.offset().left
+  mouseDrag = (pageX, animate=false) ->
+    pagePos = pageX - volumeEl.offset().left
     pagePos = Math.min(volumeEl.outerWidth(), pagePos)
     pagePos = Math.max(0, pagePos)
+
+    ratio = pagePos / volumeEl.outerWidth()
+
     if @pagePos != pagePos
       @pagePos = pagePos
-      handle.animate left: pagePos, 200
-      level.animate width: pagePos, 200
+
+      if animate
+        handle.animate left: pagePos, 200
+        level.animate width: pagePos, 200
+      else
+        handle.css left: pagePos
+        level.css width: pagePos
+
+  volumeEl.mousedown (e) =>
+    return unless e.which == 1
+    mouseDrag(e.pageX, animate=true)
+    @dragging = true
+
     false
+
+  handle.mousedown (e) =>
+    return unless e.which == 1
+    @dragging = true
+    handle.addClass "dragging"
+    mouseDrag(e.pageX)
+    false
+
+  $("body")
+    .mousemove (e) =>
+      if @dragging
+        mouseDrag(e.pageX)
+        $("body").css cursor: "pointer"
+
+    .mouseup (e) =>
+      if @dragging
+        # Finished dragging
+        @dragging = false
+        handle.removeClass "dragging"
+
+        # Revert the cursor
+        $("body").css cursor: "auto"
--- js/main.js
+++ js/main.js
@@ -1,32 +1,75 @@
 (function() {
   $(function() {
-    var handle, level, volumeEl,
+    var handle, level, mouseDrag, volumeEl,
       _this = this;
 
-    volumeEl = $('.volume');
+    volumeEl = $('.volume-track');
     level = $('.volume-level');
     handle = $('.volume-handle');
     this.pagePos = 0;
-    return volumeEl.mousedown(function(e) {
-      var pagePos;
+    mouseDrag = function(pageX, animate) {
+      var pagePos, ratio;
+
+      if (animate == null) {
+        animate = false;
+      }
+      pagePos = pageX - volumeEl.offset().left;
+      pagePos = Math.min(volumeEl.outerWidth(), pagePos);
+      pagePos = Math.max(0, pagePos);
+      ratio = pagePos / volumeEl.outerWidth();
+      if (this.pagePos !== pagePos) {
+        this.pagePos = pagePos;
+        if (animate) {
+          handle.animate({
+            left: pagePos
+          }, 200);
+          return level.animate({
+            width: pagePos
+          }, 200);
+        } else {
+          handle.css({
+            left: pagePos
+          });
+          return level.css({
+            width: pagePos
+          });
+        }
+      }
+    };
+    volumeEl.mousedown(function(e) {
+      var animate;
 
       if (e.which !== 1) {
         return;
       }
+      mouseDrag(e.pageX, animate = true);
       _this.dragging = true;
-      pagePos = e.pageX - volumeEl.offset().left;
-      pagePos = Math.min(volumeEl.outerWidth(), pagePos);
-      pagePos = Math.max(0, pagePos);
-      if (_this.pagePos !== pagePos) {
-        _this.pagePos = pagePos;
-        handle.animate({
-          left: pagePos
-        }, 200);
-        level.animate({
-          width: pagePos
-        }, 200);
-      }
       return false;
+    });
+    handle.mousedown(function(e) {
+      if (e.which !== 1) {
+        return;
+      }
+      _this.dragging = true;
+      handle.addClass("dragging");
+      mouseDrag(e.pageX);
+      return false;
+    });
+    return $("body").mousemove(function(e) {
+      if (_this.dragging) {
+        mouseDrag(e.pageX);
+        return $("body").css({
+          cursor: "pointer"
+        });
+      }
+    }).mouseup(function(e) {
+      if (_this.dragging) {
+        _this.dragging = false;
+        handle.removeClass("dragging");
+        return $("body").css({
+          cursor: "auto"
+        });
+      }
     });
   });
 
